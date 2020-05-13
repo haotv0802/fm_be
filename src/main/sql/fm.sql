a6f8fc5e0682fa50eb9d57eebc0e51684f8563de
Hao @GMS <haotv0802@gmail.com>
2020-05-13 16:52:51 +0700

    Remove unused sql files.

--- src/main/sql/fm.sql
+++ src/main/sql/fm.sql
@@ -6,275 +6,229 @@ USE `finance_management`;
 -- Table structure for table `auth_token`
 --
 DROP TABLE IF EXISTS `fm_auth_token`;
-CREATE TABLE `fm_auth_token` (
-  `id`          BIGINT AUTO_INCREMENT,
-  `token_type`  VARCHAR(45) NOT NULL,
-  `auth_object` BLOB        NOT NULL,
-  `exp_date`    DATETIME    NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `auth_token_id_unique` (`id`)
+CREATE TABLE `fm_auth_token`
+(
+    `id`          BIGINT AUTO_INCREMENT,
+    `token_type`  VARCHAR(45) NOT NULL,
+    `auth_object` BLOB        NOT NULL,
+    `exp_date`    DATETIME    NOT NULL,
+    `created`     DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `auth_token_id_unique` (`id`)
 )
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
 
 --
 -- Table structure for table `user_role`
 --
 DROP TABLE IF EXISTS `fm_user_roles`;
-CREATE TABLE `fm_user_roles` (
-  `id`        BIGINT      NOT NULL,
-  `role_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_user_role_id_unique` (`id`),
-  UNIQUE KEY `fm_user_role_role_name_unique` (`role_name`)
+CREATE TABLE `fm_user_roles`
+(
+    `id`        BIGINT      NOT NULL,
+    `role_name` VARCHAR(45) NOT NULL,
+    `created`   DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `fm_user_role_id_unique` (`id`),
+    UNIQUE KEY `fm_user_role_role_name_unique` (`role_name`)
 )
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
 
 --
 -- Table structure for table `user_table`
 --
 DROP TABLE IF EXISTS `fm_users`;
-CREATE TABLE `fm_users` (
-  `id`        BIGINT      NOT NULL,
-  `user_name` VARCHAR(45) NOT NULL,
-  `password`  VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_user_table_id_unique` (`id`),
-  UNIQUE KEY `fm_user_table_user_name_unique` (`user_name`)
+CREATE TABLE `fm_users`
+(
+    `id`        BIGINT      NOT NULL,
+    `user_name` VARCHAR(45) NOT NULL,
+    `password`  VARCHAR(45) NOT NULL,
+    `created`   DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `fm_user_table_id_unique` (`id`),
+    UNIQUE KEY `fm_user_table_user_name_unique` (`user_name`)
 )
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
 
 --
 -- Table structure for table `user_role_details`
 --
 DROP TABLE IF EXISTS `fm_user_role_details`;
-CREATE TABLE `fm_user_role_details` (
-  `id`      BIGINT NOT NULL AUTO_INCREMENT,
-  `user_id` BIGINT NOT NULL,
-  `role_id` BIGINT NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_user_role_unique` (`user_id`, `role_id`),
-  UNIQUE KEY `fm_user_role_user_id_unique` (`user_id`), #  A user has just ONLY 1 role.
-  KEY `role_id` (`role_id`),
-  CONSTRAINT `fm_user_role_details_role_id` FOREIGN KEY (`role_id`) REFERENCES `fm_user_roles` (`id`),
-  CONSTRAINT `fm_user_role_details_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`)
+CREATE TABLE `fm_user_role_details`
+(
+    `id`      BIGINT NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT NOT NULL,
+    `role_id` BIGINT NOT NULL,
+    `created` DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `fm_user_role_unique` (`user_id`, `role_id`),
+    UNIQUE KEY `fm_user_role_user_id_unique` (`user_id`), #  A user has just ONLY 1 role.
+    KEY `role_id` (`role_id`),
+    CONSTRAINT `fm_user_role_details_role_id` FOREIGN KEY (`role_id`) REFERENCES `fm_user_roles` (`id`),
+    CONSTRAINT `fm_user_role_details_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`)
 )
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 11
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 11
+    DEFAULT CHARSET = utf8;
+
+
+--
+-- Table structure for table `Banks`
+--
+DROP
+    TABLE IF EXISTS `fm_banks`;
+CREATE TABLE `fm_banks`
+(
+    `id`      BIGINT AUTO_INCREMENT,
+    `name`    VARCHAR(45) NOT NULL,
+    `address` VARCHAR(45) NOT NULL,
+    `website` VARCHAR(45),
+    `logo`    VARCHAR(45), -- Logos of Banks
+    `created` DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
 --
--- Table structure for table `messages`
+-- Table structure for table `individuals`
 --
-DROP TABLE IF EXISTS `fm_messages`;
-CREATE TABLE `fm_messages` (
-  `id`             BIGINT AUTO_INCREMENT,
-  `role_id`        BIGINT       NULL, # NULL is for anyone
-  `component_name` VARCHAR(45)  NOT NULL,
-  `message_key`    VARCHAR(45)  NOT NULL,
-  `message_en`     VARCHAR(100) NOT NULL,
-  `message_fr`     VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_messages_id_unique` (`id`),
-  UNIQUE KEY `fm_messages_role_id_component_name_key_unique` (`role_id`, `component_name`, `message_key`),
-  CONSTRAINT `fm_messages_role_id` FOREIGN KEY (`role_id`) REFERENCES `fm_user_roles` (`id`)
+DROP TABLE IF EXISTS `fm_individuals`;
+CREATE TABLE `fm_individuals`
+(
+    `id`           BIGINT AUTO_INCREMENT,
+    `first_name`   VARCHAR(45) NOT NULL,
+    `last_name`    VARCHAR(45) NOT NULL,
+    `middle_name`  VARCHAR(45),
+    `birthday`     DATE,
+    `gender`       VARCHAR(10),
+    `email`        VARCHAR(50) NOT NULL,
+    `phone_number` VARCHAR(50),
+    `income`       DOUBLE,
+    `user_id`      BIGINT      NOT NULL,
+    `created`      DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `fm_individuals_id_unique` (`id`),
+    UNIQUE KEY `fm_individuals_user_id_unique` (`user_id`), #  An individual has ONLY 1 user account.
+    UNIQUE KEY `fm_individuals_email_unique` (`email`),     #  One email is belong to only 1 individual.
+    CONSTRAINT `fm_individuals_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`)
 )
-  ENGINE = InnoDB
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
+
+--
+-- Table structure for table `Promotion category`
+--
+DROP
+    TABLE IF EXISTS `fm_promotion_categories`;
+CREATE TABLE `fm_promotion_categories`
+(
+    `id`      BIGINT AUTO_INCREMENT,
+    `name`    VARCHAR(45) NOT NULL,
+    `created` DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-
-DROP TABLE IF EXISTS `fm_earnings`;
-CREATE TABLE `fm_earnings` (
-  `id`          BIGINT       NOT NULL AUTO_INCREMENT,
-  `user_id`     BIGINT       NOT NULL,
-  `amount`      DOUBLE       NOT NULL,
-  `date`        DATETIME              DEFAULT now(),
-  `description` VARCHAR(100) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_earnings_id_unique` (`id`),
-  CONSTRAINT `fm_earnings_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`)
-)
-  ENGINE = InnoDB
+--
+-- Table structure for table `Promotion`
+--
+DROP
+    TABLE IF EXISTS `fm_promotions`;
+CREATE TABLE `fm_promotions`
+(
+    `id`          BIGINT AUTO_INCREMENT,
+    `title`       VARCHAR(45)    NOT NULL,
+    `content`     NVARCHAR(9000) NOT NULL,
+    `discount`    VARCHAR(45)    NULL, # Could be % or specific amount
+    `installment` TINYINT        NULL, # Could be % or specific amount
+    `start_date`  DATE           NOT NULL,
+    `end_date`    DATE           NOT NULL,
+    `url`         VARCHAR(200)   NOT NULL,
+    `category_id` BIGINT         NOT NULL,
+    `bank_id`     BIGINT         NOT NULL,
+    `created`     DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fm_promotions_category_id` FOREIGN KEY (`category_id`) REFERENCES `fm_promotion_categories` (`id`),
+    CONSTRAINT `fm_promotions_bank_id` FOREIGN KEY (`bank_id`) REFERENCES `fm_banks` (`id`)
+) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
+
 
 DROP TABLE IF EXISTS `fm_payment_methods`;
-CREATE TABLE `fm_payment_methods` (
-  `id`   BIGINT      NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL, # cash, credit card, debit card, master card, jbc, amex, visa credit
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_event_types_id_unique` (`id`)
+CREATE TABLE `fm_payment_methods`
+(
+    `id`      BIGINT      NOT NULL AUTO_INCREMENT,
+    `name`    VARCHAR(45) NULL, # cash, credit card, debit card, master card, jbc, amex, visa credit
+    `logo`    VARCHAR(45) NULL,
+    `created` DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `fm_event_types_id_unique` (`id`)
 )
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS `fm_cards_information`;
-CREATE TABLE `fm_cards_information` (
-  `id`            BIGINT      NOT NULL AUTO_INCREMENT,
-  `name`          VARCHAR(45) NULL, # HSBC, ANZ 123
-  `start_date`    DATE        NULL,
-  `expiry_date`   DATE        NULL,
-  `card_number`   VARCHAR(45) NULL, # last 6 digits
-  `amount`        DOUBLE      NOT NULL,
-  `card_type_id`  BIGINT      NULL,
-  `user_id`       BIGINT      NULL,
-  `is_terminated` BOOLEAN              DEFAULT FALSE,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_cards_information_id_unique` (`id`, `card_number`, `user_id`),
-  CONSTRAINT `fm_cards_information_card_type_id` FOREIGN KEY (`card_type_id`) REFERENCES `fm_payment_methods` (`id`),
-  CONSTRAINT `fm_cards_information_card_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`)
+DROP TABLE IF EXISTS `fm_money_source`;
+CREATE TABLE `fm_money_source`
+(
+    `id`            BIGINT      NOT NULL AUTO_INCREMENT,
+    `name`          VARCHAR(45) NULL, # HSBC, ANZ 123 or any name you like for such source.
+    `start_date`    DATE        NULL,
+    `expiry_date`   DATE        NULL,
+    `card_number`   VARCHAR(45) NULL, # last 6 digits
+    `amount`        DOUBLE      NOT NULL,
+    `card_type_id`  BIGINT      NULL,
+    `user_id`       BIGINT      NULL,
+    `is_terminated` BOOLEAN  DEFAULT FALSE,
+    `bank_id`       BIGINT      NOT NULL,
+    `created`       DATETIME DEFAULT now(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `fm_money_source_id_unique` (`id`, `card_number`, `user_id`),
+    CONSTRAINT `fm_money_source_type_id` FOREIGN KEY (`card_type_id`) REFERENCES `fm_payment_methods` (`id`),
+    CONSTRAINT `fm_money_source_individual_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`),
+    CONSTRAINT `fm_money_source_bank_id` FOREIGN KEY (`bank_id`) REFERENCES `fm_banks` (`id`)
 )
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS `fm_cards_payments_tracking`;
-CREATE TABLE `fm_cards_payments_tracking` (
-  `id`      BIGINT      NOT NULL,
-  `name`    VARCHAR(45) NULL, # name of payment
-  `date`    DATETIME    NULL,
-  `amount`  DOUBLE      NOT NULL,
-  `card_id` BIGINT      NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_cards_payments_tracking_id_unique` (`id`),
-  CONSTRAINT `fm_cards_payments_tracking_card_id` FOREIGN KEY (`card_id`) REFERENCES `fm_cards_information` (`id`)
+
+DROP TABLE IF EXISTS `fm_money_flow`;
+CREATE TABLE `fm_money_flow`
+(
+    `id`              BIGINT      NOT NULL AUTO_INCREMENT,
+    `user_id`         BIGINT      NOT NULL,
+    `amount`          DOUBLE      NULL, # if `is_an_event is TRUE, amount can be updated later when event is over
+    `date`            DATETIME DEFAULT now(),
+    `name`            VARCHAR(45) NOT NULL,
+    `money_source_id` BIGINT      NULL, # if `is_an_event is TRUE, card_id is NULL, or card_id is NULL means CASH payment
+    `is_deleted`      BOOLEAN  DEFAULT FALSE,
+    `is_spending`     BOOLEAN  DEFAULT FALSE,
+    `created`         DATETIME DEFAULT now(),
+    `updated`         DATETIME DEFAULT now(),
+    `note`            VARCHAR(45) NULL, # Notes for item
+    `link`            VARCHAR(45) NULL, # URL for reference
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `fm_expenses_id_unique` (`id`),
+    CONSTRAINT `fm_expenses_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`),
+    CONSTRAINT `fm_expenses_money_source_id` FOREIGN KEY (`money_source_id`) REFERENCES `fm_money_source` (`id`)
 )
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
-DROP TABLE IF EXISTS `fm_cards_invoices`;
-CREATE TABLE `fm_cards_invoices` (
-  `id`               BIGINT      NOT NULL,
-  `name`             VARCHAR(45) NULL, # name of statement
-  `amount`           DOUBLE      NOT NULL,
-  `stament_date`     DATETIME    NULL,
-  `payment_due_date` DATETIME    NULL,
-  `card_id`          BIGINT      NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_cards_payments_tracking_id_unique` (`id`),
-  CONSTRAINT `fm_cards_invoices_card_id` FOREIGN KEY (`card_id`) REFERENCES `fm_cards_information` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
-DROP TABLE IF EXISTS `fm_event_types`;
-CREATE TABLE `fm_event_types` (
-  `id`   BIGINT      NOT NULL,
-  `name` VARCHAR(45) NULL, # group sharing, traveling, group party, borrow money, pay back money
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_event_types_id_unique` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
-DROP TABLE IF EXISTS `fm_expenses`;
-CREATE TABLE `fm_expenses` (
-  `id`            BIGINT      NOT NULL AUTO_INCREMENT,
-  `user_id`       BIGINT      NOT NULL,
-  `amount`        DOUBLE      NULL, # if `is_an_event is TRUE, amount can be updated later when event is over
-  `date`          DATETIME             DEFAULT now(),
-  `place`         VARCHAR(45) NOT NULL,
-  `for_person`    VARCHAR(45) NULL, # for_person means you spend for them, not for you, so this will not be listed in monthly report.
-  # borrow someone money, if this is not important, you can let it hear,
-  # otherwise, you want to put it in reminder, should place an event for it.
-  `is_an_event`   BOOLEAN              DEFAULT FALSE,
-  `card_id`       BIGINT      NULL, # if `is_an_event is TRUE, card_id is NULL, or card_id is NULL means CASH payment
-  `is_deleted`    BOOLEAN              DEFAULT FALSE,
-  `event_type_id` BIGINT      NULL, # in case it is an event => is_an_event = true
-  `event_name`    VARCHAR(45) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_expenses_id_unique` (`id`),
-  CONSTRAINT `fm_expenses_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`),
-  CONSTRAINT `fm_expenses_event_type_id` FOREIGN KEY (`event_type_id`) REFERENCES `fm_event_types` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
-DROP TABLE IF EXISTS `fm_event_expenses`; # the event that you involve in spending money with others in the group
-# in the end, this data will be used for calculation of expense of each person
-# it is mandatory that, you involve in this even you are not a host (main person spending, but the other)
-CREATE TABLE `fm_event_expenses` (
-  `id`         BIGINT      NOT NULL AUTO_INCREMENT,
-  `amount`     DOUBLE      NOT NULL,
-  `date`       DATETIME             DEFAULT now(),
-  `place`      VARCHAR(45) NOT NULL,
-  `for_person` VARCHAR(45) NULL, # NULL means for only you.
-  `by_person`  VARCHAR(45) NULL,
-  #   `is_over`       BOOLEAN  DEFAULT FALSE,
-  #   `payment_method` VARCHAR(45) NOT NULL,
-  `card_id`    BIGINT      NULL, # pay_in_cash column is not necessary. card_id NULL means you pay in cash.
-  `expense_id` BIGINT      NOT NULL, # group sharing, traveling, group party, borrow money, pay back money
-  `is_deleted` BOOLEAN              DEFAULT FALSE,
-
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_event_expenses_id_unique` (`id`),
-  CONSTRAINT `fm_event_expenses_user_id` FOREIGN KEY (`expense_id`) REFERENCES `fm_expenses` (`id`),
-  CONSTRAINT `fm_event_expenses_card_id` FOREIGN KEY (`card_id`) REFERENCES `fm_cards_information` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
 
 --
 -- Table structure for table `error_tracking`
 --
 DROP TABLE IF EXISTS `fm_error_tracking`;
-CREATE TABLE `fm_error_tracking` (
-  `id`            BIGINT       NOT NULL AUTO_INCREMENT,
-  `error_message` VARCHAR(100) NOT NULL,
-  `stack_trace`   TEXT         NOT NULL,
-  `user`          VARCHAR(50),
-  `error_date`    DATETIME     NOT NULL DEFAULT now(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `error_tracking_id_unique` (`id`)
+CREATE TABLE `fm_error_tracking`
+(
+    `id`            BIGINT       NOT NULL AUTO_INCREMENT,
+    `error_message` VARCHAR(100) NOT NULL,
+    `stack_trace`   TEXT         NOT NULL,
+    `user`          VARCHAR(50),
+    `error_date`    DATETIME     NOT NULL DEFAULT now(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `error_tracking_id_unique` (`id`)
 )
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
---
--- Table structure for table `fm_individuals`
---
-DROP TABLE IF EXISTS `fm_individuals`;
-CREATE TABLE `fm_individuals` (
-  `id`              BIGINT AUTO_INCREMENT,
-  `first_name`      VARCHAR(45) NOT NULL,
-  `last_name`       VARCHAR(45) NOT NULL,
-  `middle_name`     VARCHAR(45),
-  `birthday`        DATE,
-  `gender`          BOOLEAN,
-  `email`           VARCHAR(50) NOT NULL,
-  `phone_number`    VARCHAR(50),
-  `official_income` DOUBLE,
-  `payment_date`    DATE,
-  `user_id`         BIGINT      NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_individuals_id_unique` (`id`),
-  UNIQUE KEY `fm_individuals_user_id_unique` (`user_id`), #  An individual has ONLY 1 user account.
-  UNIQUE KEY `fm_individuals_email_unique` (`email`), #  One email is belong to only 1 individual.
-  CONSTRAINT `fm_individuals_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
---
--- Table structure for table `fm_person_picker`
---
-DROP TABLE IF EXISTS `fm_person_picker`;
-CREATE TABLE `fm_person_picker` (
-  `id`                    BIGINT AUTO_INCREMENT,
-  `first_name`            VARCHAR(45) NOT NULL,
-  `last_name`             VARCHAR(45) NOT NULL,
-  `middle_name`           VARCHAR(45),
-  `birthday`              DATE,
-  `gender`                BOOLEAN,
-  `email`                 VARCHAR(50) NOT NULL,
-  `phone_number`          VARCHAR(50),
-  `official_income`       DOUBLE,
-  `payment_date_of_month` TINYINT,
-  `user_id`               BIGINT      NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `fm_person_picker_id_unique` (`id`),
-  UNIQUE KEY `fm_person_picker_user_id_email_unique` (`user_id`, `email`), #  An individual has ONLY 1 unique combination of user_id and email.
-  CONSTRAINT `fm_person_picker_user_id` FOREIGN KEY (`user_id`) REFERENCES `fm_users` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
