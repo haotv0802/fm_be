4c5523e791c5527272f24df618f7ecd6a9555219
haoho <hoanhhao@gmail.com>
2020-05-15 22:34:30 +0700

    Cleaning code.

--- src/main/java/fm/api/rest/promotions/PromotionResource.java
+++ src/main/java/fm/api/rest/promotions/PromotionResource.java
@@ -1,8 +1,9 @@
-package fm.api.rest.promotions.crawler;
+package fm.api.rest.promotions;
 
 
 import fm.api.rest.BaseResource;
 import fm.api.rest.promotions.crawler.interfaces.IBankPromotion;
+import fm.api.rest.promotions.crawler.interfaces.IBankPromotionCrawler;
 import fm.auth.UserDetailsImpl;
 import fm.common.beans.HeaderLang;
 import io.jsonwebtoken.lang.Assert;
@@ -13,6 +14,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ServiceLoader;
 
 /**
  * Quy created on 3/11/2020
@@ -30,12 +33,33 @@ public class PromotionResource extends BaseResource {
 
     @GetMapping("/promotions/crawler/{bankID}")
     @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
-    public String crawlingPromotion(@AuthenticationPrincipal UserDetailsImpl userDetails,
+    public void crawlingPromotion(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                     @PathVariable String bankID) {
 
         this.bankPromotion.crawl(bankID);
-        String message = "Success";
-        return message;
     }
 
+    @GetMapping("/promotions/crawlall")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
+    public void crawlAll(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+//        this.bankPromotion.crawl(bankID);
+
+//        ServiceLoader<IBankPromotionCrawler> loader = ServiceLoader.load(IBankPromotionCrawler.class);
+//        for (IBankPromotionCrawler implClass : loader) {
+//            System.out.println(implClass.getClass().getSimpleName()); // prints Dog, Cat
+//        }
+//
+//        Package[] packages = Package.getPackages();
+//        for (Package p : packages) {
+//            IBankPromotionCrawler annotation = p.getAnnotation(IBankPromotionCrawler.class);
+//            if (annotation != null) {
+//                Class<?>[]  implementations = annotation.();
+//                for (Class<?> impl : implementations) {
+//                    System.out.println(impl.getSimpleName());
+//                }
+//            }
+//        }
+
+    }
 }
